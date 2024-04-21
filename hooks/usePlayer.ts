fff3c9e512046bcb46ae2313a5ed81b91d6a910f
MDCDEV <me@mdcdev.me>
2024-04-21 02:17:55 -0500

    feat: Now the volume dont reset every time

--- hooks/usePlayer.ts
+++ hooks/usePlayer.ts
@@ -3,16 +3,20 @@ import { create } from "zustand";
 interface PlayerStore {
     ids: string[];
     activeId?: string;
+    volume: number;
     setId: (id: string) => void;
     setIds: (ids: string[]) => void;
+    setVolume: (volume: number) => void;
     reset: () => void
 }
 
 const usePlayer = create<PlayerStore>((set) => ({
     ids: [],
     activeId: undefined,
+    volume: 1,
     setId: (id: string) => set({activeId: id}),
     setIds: (ids: string[]) => set({ids: ids}),
+    setVolume: (volume: number) => set({volume}),
     reset: () => set({ids: [], activeId: undefined})
 }))
 
